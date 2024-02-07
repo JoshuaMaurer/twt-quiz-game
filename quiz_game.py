594ec953247380f2ebfd7ab573472709b9c4c487
JoshuaMaurer <maurer.joshua@gmail.com>
2024-02-06 23:50:27 -0600

    adding game start options

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,8 @@
 print("Welcome to the Wheel of Time quiz!")
+
+playing = input("Do you want to play a game? ")
+# TODO:make caps run as well
+if playing != 'yes':
+    quit()
+
+print("Fantastic! Let's continue...")
